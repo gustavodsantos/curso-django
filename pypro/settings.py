c402ef3bf29ada12526421c454e3f369eba4ba48
gustavodsantos <gustavojuniordos@hotmail.com>
2024-03-20 21:50:24 -0300

    Monitorando Erros com Sentry

--- pypro/settings.py
+++ pypro/settings.py
@@ -12,14 +12,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os.path
 from functools import partial
-from pathlib import Path
+# from pathlib import Path
 import dj_database_url
 from decouple import config, Csv
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
