29056e4838356ef4acadd3ac08a41aa2c66d02ef
Gustavo Junior dos Santos <gustavojuniordos@hotmail.com>
2024-06-24 11:17:25 -0300

    comit 02

---
+++ pypro/modulos/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from ordered_model.models import OrderedModel
+
+
+class Modulo(OrderedModel):
+    titulo = models.CharField(max_length=64)
+    publico = models.TextField(null=True)
+    descricao = models.TextField(null=True)
+
+    class Meta(OrderedModel.Meta):
+        pass
+
+    def __str__(self):
+        return self.titulo
+
